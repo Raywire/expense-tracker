b09c258a703bee4a460fa79f59924a9e4a07534b
Ali Eren Şen <58878341+Klidnos@users.noreply.github.com>
2023-05-29 11:08:22 +0300

    Delete TwitterSampleApi directory

--- TwitterSampleApi/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TwitterSampleApi.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
